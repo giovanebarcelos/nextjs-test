08aa020d22dd4ee5591d935f452f31d82a998930
Giovane Barcelos <giovanebarcelos@gmail.com>
2020-11-10 22:59:04 -0300

    new change on home

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,7 @@ import { useState } from "react";
 function Home() {
   return (
     <div>
-      <h1>Counter</h1>
-      <Counter />
-    </div>
-  );
-}
-
-function Counter() {
-  const [counter, setCounter] = useState(1);
-
-  function addCounter() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={addCounter}>Add</button>
+      <h1>First Page</h1>
     </div>
   );
 }
